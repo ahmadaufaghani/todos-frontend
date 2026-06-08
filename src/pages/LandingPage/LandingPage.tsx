2b7a52f27e708a3f21c0e881b1d42b410803322f
Ahmad Aufa Ghani <ahmadaufaghani1224@gmail.com>
2026-06-08 18:37:43 +0700

    fixing some UI

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -38,24 +38,24 @@ export default function LandingPage() {
   return (
     <div className="landing-page d-flex vh-100 justify-content-center align-items-center">
       <div className="card shadow landing-page-content text-center p-5 m-3">
-        <h1 className="mb-4">Welcome to To<span className="highlight">Do</span>.</h1>
-        <p className="text-secondary mb-4" >Boost your day to day activities with us! So you never miss a beat about your daily activity with the help of our powerful note taking app. See our achievement below to prove that our app is beneficial.</p>
+        <h4 className="mb-4 fw-bold">Welcome to To<span className="highlight">Do</span>.</h4>
+        <p className="text-secondary mb-4" >Boost your day to day activities with us and say goodbye to your skipped activity.</p>
         <div className="d-flex justify-content-center mb-5">
           <div className="row text-center">
-            <span className="fs-1 fw-medium mb-2">{stats?.total}</span>
+            <span className="fs-1 fw-bold mb-2">{stats?.total}</span>
             <span className="fw-semibold">Activities</span>
           </div>
           <div className="row text-center">
-            <span className="fs-1 fw-medium mb-2">{stats?.finish}</span>
-            <span className="fw-semibold">Finished Activities</span>
+            <span className="fs-1 fw-bold mb-2">{stats?.finish}</span>
+            <span className="fw-semibold">Finished</span>
           </div>
           <div className="row text-center">
-            <span className="fs-1 fw-medium mb-2">{stats?.usersTotal}</span>
+            <span className="fs-1 fw-bold mb-2">{stats?.usersTotal}</span>
             <span className="fw-semibold">Users</span>
           </div>
         </div>
-        <div className="d-flex justify-content-center gap-2">
-          <button className="btn btn-dark px-5 fw-semibold"  onClick={()=>navigate("/register")}>Join now!</button>
+        <div className="row">
+          <button className="btn btn-dark fw-semibold mb-3"  onClick={()=>navigate("/register")}>Register</button>
           <button className="btn btn-outline-dark fw-bold" onClick={()=>navigate("/login")}>Login</button>
         </div>
        

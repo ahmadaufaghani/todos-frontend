c705b86d1bc9dda05c89c5b671dbec5f546b87ed
Ahmad Aufa Ghani <ahmadaufaghani1224@gmail.com>
2026-06-11 12:43:22 +0700

    refining some parts

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios, {AxiosError} from 'axios';
 import {useNavigate} from 'react-router';
 import "../LandingPage/LandingPage.css"
+import { BeatLoader, BounceLoader, CircleLoader, ClipLoader, DotLoader, PacmanLoader, RotateLoader } from 'react-spinners';
+import { toast } from 'react-toastify';
 
 export default function LandingPage() {
     interface StatsPayload {
@@ -26,7 +28,7 @@ export default function LandingPage() {
             })
         } catch (err) {
             const error = (err as AxiosError<AxiosErrorPayload>).response?.data.message;
-            console.log(error);
+            toast.error("There is something wrong. Please reload the page!")
         }
     }
 
@@ -41,18 +43,28 @@ export default function LandingPage() {
         <h4 className="mb-4 fw-bold">Welcome to To<span className="highlight">Do</span>.</h4>
         <p className="text-secondary mb-4" >Boost your day to day activities with us and say goodbye to your skipped activity.</p>
         <div className="d-flex justify-content-center mb-5">
-          <div className="row text-center">
-            <span className="fs-1 fw-bold mb-2">{stats?.total}</span>
-            <span className="fw-semibold">Activities</span>
-          </div>
-          <div className="row text-center">
-            <span className="fs-1 fw-bold mb-2">{stats?.finish}</span>
-            <span className="fw-semibold">Finished</span>
-          </div>
-          <div className="row text-center">
-            <span className="fs-1 fw-bold mb-2">{stats?.usersTotal}</span>
-            <span className="fw-semibold">Users</span>
-          </div>
+          {!stats 
+            ?
+            <div className="p-4">
+              <BeatLoader className="mb-3"/>
+              <span className='fw-medium'>Loading the data, please wait!</span>
+            </div>
+            :
+            <>
+              <div className="row text-center">
+                <span className="fs-1 fw-bold mb-2">{stats?.total}</span>
+                <span className="fw-semibold">Activities</span>
+              </div>
+              <div className="row text-center">
+                <span className="fs-1 fw-bold mb-2">{stats?.finish}</span>
+                <span className="fw-semibold">Finished</span>
+              </div>
+              <div className="row text-center">
+                <span className="fs-1 fw-bold mb-2">{stats?.usersTotal}</span>
+                <span className="fw-semibold">Users</span>
+              </div>
+            </>
+          }
         </div>
         <div className="row">
           <button className="btn btn-dark fw-semibold mb-3"  onClick={()=>navigate("/register")}>Register</button>
